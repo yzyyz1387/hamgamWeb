9077b8bf8ace11a01c86e59c1cab247340a1ad3d
yzyyz1387 <1796031384@qq.com>
2024-05-19 03:24:29 +0800

    :hammer: 增加随机图片功能

--- src/components/Resource.vue
+++ src/components/Resource.vue
@@ -1,31 +1,63 @@
 <template>
-<div class="masonry" v-if="Object.keys(res).length>0">
-  <div class="item" v-for="(src, name) in res" :key="name">
-    <img :src="`https://gcore.jsdelivr.net/gh/yzyyz1387/hamgam/img/${src.url}`">
-    <h2>{{ name }}</h2>
-    <p>
-        {{ src.dec }}
-    </p>
-    <p class="contributor">
-      <span>贡献者: {{ src.contributor }}</span>
-    
-    </p>
-    <div class="share">
-      <i class="el-icon-share" aria-hidden="true" @click="SharePic(src.url)" v-if="!tempPicUrl"  title="分享图片"/></i>
-      <a href="/"  v-else><i class="el-icon-menu" aria-hidden="true" title="返回主页"/></i></a>
+<div>
+  <div class="btn change-mode-btn"  @click="changeMode" >
+    <i class="el-icon-magic-stick" aria-hidden="true" title="随机模式"></i>&nbsp;{{ !randomMode? "随机模式" : "普通模式" }}
+  </div>
+  <div class="btn one-more-btn" v-if="randomMode || Object.keys(res).length==1" @click="oneMore">
+     <i class="el-icon-thumb" aria-hidden="true" title="再来一张"></i>&nbsp;再来一张
+  </div>
+  <div class="outer" v-if="!randomMode">
+    <div class="masonry" v-if="Object.keys(res).length>0">
+      <div class="item" v-for="(src, name) in res" :key="name">
+        <img :src="config.cdn + src.url">
+        <h2>{{ name }}</h2>
+        <p>
+            {{ src.dec }}
+        </p>
+        <p class="contributor">
+          <span>贡献者: {{ src.contributor }}</span>
+        
+        </p>
+        <div class="share">
+          <i class="el-icon-share" aria-hidden="true" @click="SharePic(src.url)" v-if="!tempPicUrl"  title="分享图片"></i>
+          <a href="/"  v-else><i class="el-icon-menu" aria-hidden="true" title="返回主页"></i></a>
 
+        </div>
+      </div>
+    </div>
+    <div class="masonry" v-else>
+        <div class="item load" >
+        <img src="../assets/loading.gif" alt="">
+        <h2>加载中...</h2>
+      </div>
     </div>
   </div>
-</div>
-<div class="masonry" v-else>
-    <div class="item load" >
-    <img src="../assets/loading.gif" alt="">
-    <h2>加载中...</h2>
+  <div class="outer" v-else>
+      <div class="show-one">
+        <div class="item item-one">
+                  <img :src="config.cdn + randomData.url">
+        <h2>{{ randomData.name }}</h2>
+        <p>
+            {{ randomData.dec }}
+        </p>
+        <p class="contributor">
+          <span>贡献者: {{ randomData.contributor }}</span>
+        
+        </p>
+        <div class="share">
+          <i class="el-icon-share" aria-hidden="true" @click="SharePic(randomData.url)" v-if="!tempPicUrl"  title="分享图片"></i>
+          <a href="/"  v-else><i class="el-icon-menu" aria-hidden="true" title="返回主页"></i></a>
+
+        </div>
+        </div>
+      </div>
   </div>
 </div>
 </template>
 
 <script>
+import config from '../config.js';
+
 export default {
     name: "Resource",
     props: {
@@ -36,9 +68,13 @@ export default {
     },
     data() {
         return {
+          config: config,
             res: {
             },
-            tempPicUrl:""
+            randomData:{},
+            tempPicUrl:"",
+            randomMode:false
+
         }
     },
     created(){
@@ -51,6 +87,22 @@ mounted() {
   });
 },
     methods: {
+      oneMore(){
+        this.getRandomPic();
+      },
+      getRandomPic() {
+        let keys = Object.keys(this.res);
+        let randomKey = keys[Math.floor(Math.random() * keys.length)];
+        let temp_ = this.res[randomKey];
+        temp_['name'] = randomKey;
+        this.randomData = temp_;
+      },
+      changeMode() {
+        this.randomMode = !this.randomMode;
+        if (this.randomMode) {
+          this.getRandomPic();
+        }
+      },
       SharePic(url) {
         const input = document.createElement('input');
         document.body.appendChild(input);
@@ -70,6 +122,11 @@ mounted() {
     updateRes() {
     const picUrl = this.$route.params.picUrl;
     if (picUrl) {
+      if (picUrl==='random'){
+        this.randomMode = true;
+        this.getRandomPic();
+        return;
+      }
       for (let key in this.res) {
         if (this.res[key].url==picUrl ) {
           this.tempPicUrl = picUrl;
@@ -82,7 +139,7 @@ mounted() {
     }
   },
       async updateData() {
-        let url = "https://jsd.seeku.site/yzyyz1387/hamgam/pic_res.json";
+        let url = config.res;
         try {
           let res = await axios.get(url);
           if (res.data["url"]) {
@@ -115,7 +172,41 @@ a{
   margin: 20px auto;
   columns: 4;
   column-gap: 30px;
+  position: relative;
 
+}
+.btn{
+  z-index: 999;
+  height: 45px;
+  cursor: pointer;
+  user-select: none;
+  background-color: #3898fc;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.3s ease;
+  padding: 0 10px;
+  color: #fff;
+}
+.change-mode-btn,
+.one-more-btn {
+  border-radius: 15px;
+  position: fixed;
+  right: 20px;
+}
+
+.change-mode-btn {
+  bottom: 20px;
+}
+
+.one-more-btn {
+  bottom: 80px;
+}
+.btn i{
+  font-size: 1.5em;
+  font-weight: 900;
+  color: #fff;
 }
 .item {
   width: 100%;
@@ -125,9 +216,10 @@ a{
   border: 1px solid #f0f0f0;
   background-color: #fff;
   position: relative;
+  box-shadow: -2px 7px 8px 6px rgba(0, 0, 0, 0.1); 
 }
 .item:hover{
-  box-shadow: -2px 7px 8px 6px rgba(0, 0, 0, 0.1); 
+  box-shadow: 5px 8px 7px 3px rgb(0 0 0 / 36%);
   transform: translateY(-5px);
 }
 
@@ -143,6 +235,20 @@ a{
 .item P {
   color: #555;
   margin: 0 15px 15px 15px;
+}
+.show-one{
+  display: flex;
+  justify-content: center;
+
+}
+.item-one{
+  width: 50%;
+  margin: 20px;
+  border: 1px solid #f0f0f0;
+  background-color: #fff;
+  position: relative;
+  box-shadow: -2px 7px 8px 6px rgba(0, 0, 0, 0.1); 
+
 }
 .contributor {
   color: #555;
@@ -185,11 +291,29 @@ a{
     columns: 2;
     column-gap: 20px;
   }
+    .item-one {
+    width: 96vw;
+  }
+  .change-mode-btn {
+    bottom: 80px;
+    }
+    .one-more-btn {
+      bottom: 140px;
+    }
 }
 @media screen and (max-width: 767.98px) {
   .masonry {
     width: 96vw;
     columns: 1;
   }
+  .item-one {
+    width: 96vw;
+  }
+  .change-mode-btn {
+    bottom: 80px;
+    }
+    .one-more-btn {
+      bottom: 140px;
+    }
 }
 </style>
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export default {"res":"https://jsd.seeku.site/yzyyz1387/hamgam/pic_res.json","cdn":"https://gcore.jsdelivr.net/gh/yzyyz1387/hamgam/img/"};
