e362d780e817f6b269a78d72c05388be0ab8ab67
yzyyz1387 <1796031384@qq.com>
2024-05-20 14:29:57 +0800

    :bug: 修复直接访问某一图片页面的逻辑

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export default {"res":"https://jsd.seeku.site/yzyyz1387/hamgam/pic_res.json","cdn":"https://gcore.jsdelivr.net/gh/yzyyz1387/hamgam/img/"};
+export default {
+    "res":"https://jsd.seeku.site/yzyyz1387/hamgam/pic_res.json",
+    "cdn":"https://gcore.jsdelivr.net/gh/yzyyz1387/hamgam/img/",
+};
